fa12bc32f9e22b9045a4635d06b4e2ddabbd747a
ELKENTAOUI HAMMAM <elkentaoui.ha@gmail.com>
2025-03-22 06:57:16 +0000

    first commit

---
+++ app/src/main/java/com/example/tp4/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.example.tp4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        logo = findViewById(R.id.logo); logo.animate().rotation(360f).setDuration(2000);
+        logo.animate().scaleX(0.5f).scaleY(0.5f).setDuration(3000);
+        logo.animate().translationYBy(1000f).setDuration(2000);
+        logo.animate().alpha(0f).setDuration(6000);
+
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(SplashActivity.this, ListActivity.class);
+                    startActivity(intent);
+                    SplashActivity.this.finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        t1.start();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.finish();
+    }
+}
